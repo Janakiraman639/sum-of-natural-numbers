525bfc0d82c0bedb615f312130550870559fdfef
Janakiraman639 <janakiramana639@gmail.com>
2025-01-24 20:40:07 +0530

    Update natural number.py

--- natural number.py
+++ natural number.py
@@ -7,3 +7,9 @@ n=int(input("enter the number:"))
 result = sum_of_natural_no(n)
 print(f"the sum of first {n} natural no is:{result}")
 
+or
+
+n = int(input("enter the number:"))
+c = n*n+1 / 2 ;
+print("The sum of n natural no is ",c)
+
